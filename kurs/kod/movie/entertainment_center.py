0e2fc6283d5b2a01f8316f71e85e8cbd93207ba4
Piotr Wełpa <pwelpa@piotr.domek>
2017-07-09 14:48:40 +0200

    add Movies class implementation add movies objects to entertainment_center.py

---
+++ kurs/kod/movie/entertainment_center.py
@@ -0,0 +1,34 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toy that come to life",
+                        "http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alein planet",
+                     "http://www.impawards.com/2009/posters/avatar_xlg.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+
+matrix = media.Movie("Matrix",
+                     "Hacker figured out that he lives in simulation",
+                     "https://en.wikipedia.org/wiki/File:The_Matrix_Poster.jpg",
+                     "https://www.youtube.com/watch?v=vKQi3bBA1y8")
+
+school_of_rock = media.Movie("School of Rock",
+                             "Using rock music to learn",
+                             "https://en.wikipedia.org/wiki/File:School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=5afGGGsxvEA")
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "Going back in time to meet authors",
+                                "http://is1.mzstatic.com/image/thumb/Video/v4/e0/0f/d2/e00fd25b-be6a-c6a1-f1b3-e05b4dc1f2e7/source/1200x630bb.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+
+hunger_games = media.Movie("Hunger Games",
+                           "A really real reality show",
+                           "http://cdn.collider.com/wp-content/uploads/the-hunger-games-poster.jpg",
+                           "https://www.youtube.com/watch?v=mfmrPu43DF8")
